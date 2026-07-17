bde4a0005cdb841f6731752cb096de67b9f98714
SolankiSir <mayanksolanki0004@gmail.com>
2026-07-17 22:14:29 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Wedding Date
+const weddingDate = new Date("December 12, 2026 09:00:00").getTime();
+
+setInterval(function () {
+
+    const now = new Date().getTime();
+
+    const distance = weddingDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+
+    document.getElementById("hours").innerHTML = hours;
+
+    document.getElementById("minutes").innerHTML = minutes;
+
+    document.getElementById("seconds").innerHTML = seconds;
+
+},1000);
